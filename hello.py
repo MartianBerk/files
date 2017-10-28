f539c3561e1a836dec1d095295208a6c74cde124
MartianBerk <baker.bsc@gmail.com>
2017-10-28 17:26:54 +0100

    Change

--- hello.py
+++ hello.py
@@ -10,16 +10,3 @@ for file in os.listdir(path):
 	if attributes.st_mtime > tmp:
 		tmp = attributes.st_mtime
 		fileMostRecent = file
-
-print(fileMostRecent)
-
-# Delete any file in the Downloads folder which hasn't been modified in over 2 weeks
-now = time.time()
-cutoff = now - (60*60*24*14) # 2 weeks ago
-for file in os.listdir(path):
-	attributes = os.stat(os.path.join(path, file))
-	if attributes.st_mtime < cutoff:
-		try:
-			os.remove(os.path.join(path, file))
-		except OSError:
-			print(OSError.strerror)
